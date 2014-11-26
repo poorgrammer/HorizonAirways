28f25f8fc2c1563f3c588fba4c9264c784a9499b
jermie <jermiedomingo@gmail.com>
2014-11-26 11:26:28 +0800

    impl

--- HorizonAirways/src/com/px1/horizonairways/daimpl/ReservationDA.java
+++ HorizonAirways/src/com/px1/horizonairways/daimpl/ReservationDA.java
@@ -17,7 +17,7 @@ import com.px1.horizonairways.entity.FlightDetails;
 import com.px1.horizonairways.entity.FlightId;
 import com.px1.horizonairways.entity.FlightSchedule;
 import com.px1.horizonairways.entity.Passenger;
-import com.px1.horizonairways.entity.ReserveFlight;
+import com.px1.horizonairways.entity.ReservedFlight;
 
 public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 		ReserveFlightsDA {
@@ -25,6 +25,8 @@ public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 	private static final String GET_ALL_FLIGHT_SCHEDULE = "SELECT * FROM flightdetailsindays";
 	private static final String GET_ALL_FLIGHT_DETAILS = "SELECT * FROM flightdetails";
 	private static final String GET_ALL_FLIGHT_DETAILS_BY_FLIGHTID = "SELECT * FROM flightdetails WHERE FlightNo = ? AND FlightDate = ?";
+	private static final String GET_ALL_RESERVED_FLIGHTS_BY_PNR = "SELECT * FROM reservedflights WHERE pnrno = ?";
+
 	@Override
 	public List<FlightSchedule> getAllFlightSchedule() {
 
@@ -33,9 +35,9 @@ public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 		try {
 			stat = DatabaseConnector.getConnection().createStatement();
 			ResultSet rs = stat.executeQuery(GET_ALL_FLIGHT_SCHEDULE);
-			
+
 			while (rs.next()) {
-				
+
 				String flightNo = rs.getString(1);
 				String sectorId = rs.getString(2);
 				String weekDayOne = rs.getString(3);
@@ -76,9 +78,9 @@ public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 		try {
 			stat = DatabaseConnector.getConnection().createStatement();
 			ResultSet rs = stat.executeQuery(GET_ALL_FLIGHT_SCHEDULE);
-			
+
 			while (rs.next()) {
-				
+
 				String flightNo = rs.getString(1);
 				String sectorId = rs.getString(2);
 				String aircraftDescription = rs.getString(3);
@@ -91,8 +93,8 @@ public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 
 				FlightDetails flightDetails = new FlightDetails(flightNo,
 						sectorId, flightDate, aircraftDescription, depTime,
-						arrTime, firstClassSeatsAvailable, businessClassSeatsAvailable,
-						economyClassSeatsAvailable);
+						arrTime, firstClassSeatsAvailable,
+						businessClassSeatsAvailable, economyClassSeatsAvailable);
 
 				flightDetailsList.add(flightDetails);
 
@@ -100,34 +102,35 @@ public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			
+
 			if (stat != null)
-				
+
 				try {
 					stat.close();
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 		}
-		
+
 		return flightDetailsList;
-		
+
 	}
 
 	@Override
 	public List<FlightDetails> getFlightDetails(FlightId flightId) {
 
 		List<FlightDetails> flightDetailsList = new ArrayList<FlightDetails>();
-		
+
 		PreparedStatement ps = null;
 		try {
-			ps = DatabaseConnector.getConnection().prepareStatement(GET_ALL_FLIGHT_DETAILS_BY_FLIGHTID);
+			ps = DatabaseConnector.getConnection().prepareStatement(
+					GET_ALL_FLIGHT_DETAILS_BY_FLIGHTID);
 			ps.setString(0, flightId.getFlightNo());
 			ps.setDate(1, new java.sql.Date(flightId.getFlightDate().getTime()));
-			ResultSet rs =  ps.executeQuery();
-			
-			while(rs.next()){
-				
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+
 				String flightNo = rs.getString(1);
 				String sectorId = rs.getString(2);
 				String aircraftDescription = rs.getString(3);
@@ -144,16 +147,15 @@ public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 						economyClassFare);
 
 				flightDetailsList.add(flightDetails);
-				
+
 			}
 		} catch (SQLException e) {
-		
+
 			e.printStackTrace();
-		}
-		finally{
-			
-			if(ps!=null){
-				
+		} finally {
+
+			if (ps != null) {
+
 				try {
 					ps.close();
 				} catch (SQLException e) {
@@ -161,25 +163,78 @@ public class ReservationDA implements FlightDetailsDA, PassengerDetailsDA,
 				}
 			}
 		}
+
+		return flightDetailsList;
+
+	}
+
+	@Override
+	public List<Passenger> getAllPassengers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ReservedFlight> getAllReservedFlights() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ReservedFlight> getReservedFlights(String pnr) {
+
+		List<ReservedFlight> reservedFlights = new ArrayList<ReservedFlight>();
+		PreparedStatement ps = null;
+
+		try {
+			ps = DatabaseConnector.getConnection().prepareStatement(
+					GET_ALL_RESERVED_FLIGHTS_BY_PNR);
+			ps.setString(0, pnr);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+
+				String PNRno = rs.getString(1);
+				String FlightNo = rs.getString(2);
+				Date flightDate = rs.getDate(3);
+				String seatNo = rs.getString(4);
+				String seatClass = rs.getString(5);
+				String mealPreference = rs.getString(6);
+				String SSR = rs.getString(7);
+
+				ReservedFlight reservedFlight = new ReservedFlight(PNRno,
+						FlightNo, flightDate, seatNo, seatClass,
+						mealPreference, SSR);
+				reservedFlights.add(reservedFlight);
+
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		finally {
+			if (ps != null) {
+				try {
+					ps.close();
+				} catch (SQLException e) {
+
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return reservedFlights;
+
+	}
+
+	
+	
+	@Override
+	public List<Passenger> getAllPassengersByFlight(FlightId id) {
 		
-	return flightDetailsList;
-
-	}
-	
-	
-	
-	@Override
-	public List<Passenger> getAllPassenger() {
-		// TODO Auto-generated method stub
+		
+		
 		return null;
 	}
-
-	@Override
-	public List<ReserveFlight> getAllReserveFlights() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
 
 }
